b87dfec39c84fc9798a327046c650e73dde9c3fb
Yangfan He <olian644@gmail.com>
2020-11-07 22:53:36 +0800

    新增【what-to-eat】项目

--- README.md
+++ README.md
@@ -6,13 +6,15 @@
 <br/>
 <br/>
 ### 主题要求
-主题色：#181717  
-次要主题色：#4472C4  
-背景色：#E6E6E6  
-字体色（深）：#343A40  
-字体色（浅）：#F8F9FA
+|||
+|----|----|
+|主题色| #181717|
+|次要主题色|#4472C4|
+|主要背景色|#E6E6E6|
+|字体色（深）|#343A40|
+|字体色（浅）|#F8F9FA|
 ### 当前项目
 1. 可视神经网络（neural-network） 来源：(已改造）[Deep playground](https://github.com/tensorflow/playground)
 2. 狗屁不通文章生成器（bullshit-generator） 来源：(已改造）[BullshitGenerator](https://github.com/menzi11/BullshitGenerator)
-3. 加密（encrypt） 来源：原创
-4. 初音音乐生成器（mikutap） 来源：[mikutap](https://aidn.jp/mikutap)
+3. 加密（encrypt） 来源：原创（引用 *crypto.js* 库）
+4. 初音音乐生成器（mikutap） 来源：[MikuTap](https://aidn.jp/mikutap)
---
+++ list/what-to-eat/script.js
@@ -0,0 +1,90 @@
+//初始化地图
+window.$ = function (selector) {
+    return document.querySelector(selector)
+}
+
+const Map = new BMap.Map("map")
+let Point = new BMap.Point(112.923100, 27.910014)
+Map.enableScrollWheelZoom()
+Map.centerAndZoom(Point, 18)
+Map.addOverlay(new BMap.Marker(Point))
+//最近搜索参数及结果
+let lastSearch = {
+    point: Point,
+    type: '',
+    radius: 0,
+    list: [],
+    index: 0,
+    init: function (p, t, r, l) {
+        this.point = p
+        this.type = t
+        this.radius = r
+        this.list = l
+        this.index = 0
+    },
+    getNext: function () {
+        if (this.index === this.list.length)
+            this.index = 0
+        return this.list[this.index++]
+    }
+}
+
+function whatToEat() {
+    let btn = $('#wte'), resultPane = $('#result'), t = $('#type').value, r = parseInt($('#radius').value), list = []
+    btn.setAttribute('disabled', 'true')
+    resultPane.style.opacity = "0"
+
+    if (Point === lastSearch.point && r === lastSearch.radius && t === lastSearch.type) {
+        showPoi(lastSearch.getNext())
+        btn.removeAttribute('disabled')
+        resultPane.style.opacity = "1"
+    } else {
+        let options = {
+            onSearchComplete: function (results) {
+                if (local.getStatus() === BMAP_STATUS_SUCCESS) {
+                    for (let i = 0; i < results.getCurrentNumPois(); i++) {
+                        let item = results.getPoi(i)
+                        list.push({
+                            name: item.title,
+                            address: item.address,
+                            url: item.detailUrl
+                        });
+                    }
+                    if (results.getPageIndex() < results.getNumPages() - 1)
+                        local.gotoPage(results.getPageIndex() + 1)
+                    else {
+                        lastSearch.init(Point, t, r, list.shuffle())
+                        showPoi(lastSearch.getNext())
+                        btn.removeAttribute('disabled')
+                        resultPane.style.opacity = "1"
+                    }
+                } else {
+                    alert('别吧，这个鬼地方毛都没得吃╮(╯▽╰)╭')
+		    btn.removeAttribute('disabled')
+                }
+            }
+        }
+        let local = new BMap.LocalSearch(Map, options)
+        local.searchNearby(t, Point, r)
+    }
+}
+
+//展示结果
+function showPoi(target) {
+    $('#result-name').innerText = target.name
+    $('#result-address').innerText = target.address
+    $('#result-url').setAttribute('href', target.url)
+}
+
+//数组乱序函数
+Array.prototype.shuffle = function () {
+    let array = this
+    let m = array.length, t, i
+    while (m) {
+        i = Math.floor(Math.random() * m--)
+        t = array[m]
+        array[m] = array[i]
+        array[i] = t
+    }
+    return array
+}
